0b3310cefb8f8b7bc596a85940d1620eab416ae3
septor <patrickweaver@gmail.com>
2015-11-06 11:57:52 -0500

    some cleanup

--- admin_config.php
+++ admin_config.php
@@ -1,7 +1,11 @@
 <?php
-
-// Generated e107 Plugin Admin Area 
-
+/* 
+* Form Builder - A form building plugin for e107v2 
+*
+* Copyright (C) 2015 Patrick Weaver (http://trickmod.com/)
+* For additional information refer to the README.md file.
+*
+*/
 require_once('../../class2.php');
 if (!getperms('P')) 
 {
@@ -9,13 +13,9 @@ if (!getperms('P'))
 	exit;
 }
 
-
-
 class formbuilder_adminArea extends e_admin_dispatcher
 {
-
 	protected $modes = array(	
-	
 		'main'	=> array(
 			'controller' 	=> 'forms_ui',
 			'path' 			=> null,
@@ -23,27 +23,21 @@ class formbuilder_adminArea extends e_admin_dispatcher
 			'uipath' 		=> null
 		),
 		
-
 		'cat'	=> array(
 			'controller' 	=> 'form_results_ui',
 			'path' 			=> null,
 			'ui' 			=> 'form_results_form_ui',
 			'uipath' 		=> null
 		),
-		
-
 	);	
-	
 	
 	protected $adminMenu = array(
 
-		'main/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
-		'main/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
+		'main/list'			=> array('caption'=> 'Manage Forms', 'perm' => 'P'),
+		'main/create'		=> array('caption'=> 'Create New Form', 'perm' => 'P'),
 
-		'cat/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
-		'cat/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
-
-		// 'main/custom'		=> array('caption'=> 'Custom Page', 'perm' => 'P')
+		'cat/list'			=> array('caption'=> 'Manage Results', 'perm' => 'P'),
+		'cat/create'		=> array('caption'=> 'Create Result', 'perm' => 'P'),
 	);
 
 	protected $adminMenuAliases = array(
@@ -53,10 +47,6 @@ class formbuilder_adminArea extends e_admin_dispatcher
 	protected $menuTitle = 'Form Builder';
 }
 
-
-
-
-				
 class forms_ui extends e_admin_ui
 {
 			
@@ -76,41 +66,197 @@ class forms_ui extends e_admin_ui
 	
 		protected $listOrder		= 'form_id DESC';
 	
-		protected $fields 		= array (  'checkboxes' =>   array ( 'title' => '', 'type' => null, 'data' => null, 'width' => '5%', 'thclass' => 'center', 'forced' => '1', 'class' => 'center', 'toggle' => 'e-multiselect',  ),
-		  'form_id' =>   array ( 'title' => LAN_ID, 'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_name' =>   array ( 'title' => LAN_TITLE, 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_class' =>   array ( 'title' => LAN_USERCLASS, 'type' => 'userclass', 'data' => 'int', 'width' => 'auto', 'batch' => true, 'filter' => true, 'inline' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_once' =>   array ( 'title' => 'Once', 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_viewclass' =>   array ( 'title' => 'Viewclass', 'type' => 'userclass', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_editclass' =>   array ( 'title' => 'Editclass', 'type' => 'userclass', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_mailto' =>   array ( 'title' => 'Mailto', 'type' => 'email', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_forum' =>   array ( 'title' => 'Forum', 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_save_results' =>   array ( 'title' => 'Results', 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_user' =>   array ( 'title' => LAN_AUTHOR, 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_parms' =>   array ( 'title' => 'Parms', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_message' =>   array ( 'title' => 'Message', 'type' => 'textarea', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_submit_message' =>   array ( 'title' => 'Message', 'type' => 'textarea', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'form_lastfnum' =>   array ( 'title' => 'Lastfnum', 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'options' =>   array ( 'title' => LAN_OPTIONS, 'type' => null, 'data' => null, 'width' => '10%', 'thclass' => 'center last', 'class' => 'center last', 'forced' => '1',  ),
+		protected $fields = array (
+			'checkboxes' =>  array (
+				'title' => '',
+				'type' => null,
+				'data' => null,
+				'width' => '5%',
+				'thclass' => 'center',
+				'forced' => '1',
+				'class' => 'center',
+				'toggle' => 'e-multiselect',
+		   	),
+			'form_id' => array (
+				'title' => LAN_ID,
+				'data' => 'int',
+				'width' => '5%',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_name' => array (
+				'title' => LAN_TITLE,
+				'type' => 'text',
+				'data' => 'str',
+				'width' => 'auto',
+				'inline' => true,
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '', 
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_class' => array (
+				'title' => LAN_USERCLASS,
+				'type' => 'userclass',
+				'data' => 'int',
+				'width' => 'auto',
+				'batch' => true,
+				'filter' => true,
+				'inline' => true,
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_once' => array (
+				'title' => 'Once',
+				'type' => 'boolean',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_viewclass' => array (
+				'title' => 'Viewclass',
+				'type' => 'userclass',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_editclass' => array (
+				'title' => 'Editclass',
+				'type' => 'userclass',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_mailto' => array (
+				'title' => 'Mailto',
+				'type' => 'email',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_forum' => array (
+				'title' => 'Forum',
+				'type' => 'boolean',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_save_results' => array (
+				'title' => 'Results',
+				'type' => 'boolean',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_user' => array (
+				'title' => LAN_AUTHOR,
+				'type' => 'text',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_parms' => array (
+				'title' => 'Parms',
+				'type' => 'text',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'form_message' => array (
+				'title' => 'Message',
+				'type' => 'textarea',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left', 
+		   	),
+			'form_submit_message' => array (
+				'title' => 'Message',
+				'type' => 'textarea',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left', 
+		   	),
+			'form_lastfnum' => array (
+				'title' => 'Lastfnum',
+				'type' => 'boolean',
+				'data' => 'int',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left', 
+		   	),
+			'options' => array (
+				'title' => LAN_OPTIONS,
+				'type' => null,
+				'data' => null,
+				'width' => '10%',
+				'thclass' => 'center last',
+				'class' => 'center last',
+				'forced' => '1', 
+		   	),
 		);		
 		
 		protected $fieldpref = array('form_name', 'form_class');
-		
 
 	//	protected $preftabs        = array('General', 'Other' );
 		protected $prefs = array(
 		); 
-
 	
 		public function init()
 		{
 			// Set drop-down values (if any). 
-	
 		}
-
 		
 		// ------- Customize Create --------
-		
 		public function beforeCreate($new_data)
 		{
 			return $new_data;
@@ -126,9 +272,7 @@ class forms_ui extends e_admin_ui
 			// do something		
 		}		
 		
-		
 		// ------- Customize Update --------
-		
 		public function beforeUpdate($new_data, $old_data, $id)
 		{
 			return $new_data;
@@ -143,9 +287,8 @@ class forms_ui extends e_admin_ui
 		{
 			// do something		
 		}		
-		
 			
-	/*	
+		/*	
 		// optional - a custom page.  
 		public function customPage()
 		{
@@ -153,18 +296,12 @@ class forms_ui extends e_admin_ui
 			return $text;
 			
 		}
-	*/
-			
+		*/
 }
-				
-
 
 class forms_form_ui extends e_admin_form_ui
 {
-
 }		
-		
-
 				
 class form_results_ui extends e_admin_ui
 {
@@ -185,26 +322,80 @@ class form_results_ui extends e_admin_ui
 	
 		protected $listOrder		= 'results_id DESC';
 	
-		protected $fields 		= array (  'checkboxes' =>   array ( 'title' => '', 'type' => null, 'data' => null, 'width' => '5%', 'thclass' => 'center', 'forced' => '1', 'class' => 'center', 'toggle' => 'e-multiselect',  ),
-		  'results_id' =>   array ( 'title' => LAN_ID, 'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'results_datestamp' =>   array ( 'title' => LAN_DATESTAMP, 'type' => 'datestamp', 'data' => 'int', 'width' => 'auto', 'filter' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'results_form_id' =>   array ( 'title' => LAN_ID, 'type' => 'number', 'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'results_results' =>   array ( 'title' => 'Results', 'type' => 'textarea', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'options' =>   array ( 'title' => LAN_OPTIONS, 'type' => null, 'data' => null, 'width' => '10%', 'thclass' => 'center last', 'class' => 'center last', 'forced' => '1',  ),
+		protected $fields = array ( 
+			'checkboxes' =>  array (
+				'title' => '',
+				'type' => null,
+				'data' => null,
+				'width' => '5%',
+				'thclass' => 'center',
+				'forced' => '1',
+				'class' => 'center',
+				'toggle' => 'e-multiselect',
+		  	),
+			'results_id' => array (
+				'title' => LAN_ID,
+				'data' => 'int',
+				'width' => '5%',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		  	),
+			'results_datestamp' => array (
+				'title' => LAN_DATESTAMP,
+				'type' => 'datestamp',
+				'data' => 'int',
+				'width' => 'auto',
+				'filter' => true,
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left', 
+		   	),
+			'results_form_id' => array (
+				'title' => LAN_ID,
+				'type' => 'number',
+				'data' => 'int',
+				'width' => '5%',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left',
+		   	),
+			'results_results' => array (
+				'title' => 'Results', 
+				'type' => 'textarea',
+				'data' => 'str',
+				'width' => 'auto',
+				'help' => '',
+				'readParms' => '',
+				'writeParms' => '',
+				'class' => 'left',
+				'thclass' => 'left', 
+		   	),
+			'options' => array (
+				'title' => LAN_OPTIONS,
+				'type' => null,
+				'data' => null,
+				'width' => '10%',
+				'thclass' => 'center last',
+				'class' => 'center last',
+				'forced' => '1', 
+		   	),
 		);		
 		
 		protected $fieldpref = array('results_datestamp');
 		
-	
 		public function init()
 		{
 			// Set drop-down values (if any). 
-	
 		}
-
 		
 		// ------- Customize Create --------
-		
 		public function beforeCreate($new_data)
 		{
 			return $new_data;
@@ -220,9 +411,7 @@ class form_results_ui extends e_admin_ui
 			// do something		
 		}		
 		
-		
 		// ------- Customize Update --------
-		
 		public function beforeUpdate($new_data, $old_data, $id)
 		{
 			return $new_data;
@@ -237,9 +426,7 @@ class form_results_ui extends e_admin_ui
 		{
 			// do something		
 		}		
-		
-			
-	/*	
+		/*	
 		// optional - a custom page.  
 		public function customPage()
 		{
@@ -247,17 +434,12 @@ class form_results_ui extends e_admin_ui
 			return $text;
 			
 		}
-	*/
-			
+		*/
 }
-				
-
 
 class form_results_form_ui extends e_admin_form_ui
 {
-
 }		
-		
 		
 new formbuilder_adminArea();
 
@@ -266,5 +448,4 @@ e107::getAdminUI()->runPage();
 
 require_once(e_ADMIN."footer.php");
 exit;
-
 ?>
